82bd67360d26169d51d17c0978adca20e79bb745
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2024-02-15 20:46:02 +0200

    EFTI-56 - start/down local environment

---
+++ docker/flyway.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+cd $(dirname $0)
+
+if [ -z "$1" ]
+then
+  echo "Command is missing!"
+  echo "Usage: $0 [migrate or clean]"
+  exit 1
+fi
+
+cd ../db
+
+export EFTI_DB_PASSWORD="efti"
+export EFTI_DB_GATEWAY_PASSWORD="efti"
+export EFTI_DB_HOST="localhost"
+
+# Don't run test migrations to template db: efti_template
+EFTI_DB_DATABASE_NAME="efti_template" ./db.sh $1
+
+# Run test migrations to efti_dev
+EFTI_DB_DATABASE_NAME="efti_dev" ./db.sh $1 test
---
+++ docker/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+cd $(dirname $0)
+
+chmod a+rx initdb
+chmod a+r initdb/01-init.sql
+
+docker compose up --detach
+
+# Wait naively for PostgreSQL to start
+sleep 2
+
+echo "Migrate database"
+./flyway.sh migrate
