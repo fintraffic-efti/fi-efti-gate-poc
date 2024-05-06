8adf14130981dce9ddb0c2093c1afd28507a7b87
Mikko Suniala <mikko.suniala@solita.fi>
2024-05-06 12:38:15 +0300

    EFTI-226 create client certs for two platforms, add end_user

--- db/src/test/sql/migration/repeatable/r-test-platforms.sql
+++ db/src/test/sql/migration/repeatable/r-test-platforms.sql
@@ -2,7 +2,8 @@ set xxxx.client_id = 0;
 set xxxx.client_service_uri = 'database.xxxx';
 
 insert into end_user (id, role_id, name, platform_url) values
-  (-10, 0, 'Test platform', 'http://localhost:9080/api/v1'),
+  (-10, 0, 'Test platform fi 1', 'http://localhost:9080/api/v1/fi-platform-1'),
+  (-12, 0, 'Test platform fi 2', 'http://localhost:9080/api/v1/fi-platform-2'),
   (-11, 0, 'Mock platform', 'http://localhost:8091/api/v1')
 on conflict (id) do update set
   role_id = excluded.role_id,
--- docker/alb/create-certificates.sh
+++ docker/alb/create-certificates.sh
@@ -27,27 +27,32 @@ openssl req -new -x509 -days 365 -key aap-ca.key -out aap-ca.crt -subj '/C=FI/O=
 
 echo "Create test private keys"
 openssl genrsa -out gate-efti-localhost.key 4096
-openssl genrsa -aes256 -passout pass:$pwd -out test-platform.key 4096
+openssl genrsa -aes256 -passout pass:$pwd -out test-platform-fi-1.key 4096
+openssl genrsa -aes256 -passout pass:$pwd -out test-platform-fi-2.key 4096
 openssl genrsa -aes256 -passout pass:$pwd -out mock-platform.key 4096
 openssl genrsa -aes256 -passout pass:$pwd -out aap.key 4096
 
 echo "Create certificate requests"
 openssl req -new -key gate-efti-localhost.key -out gate-efti-localhost.csr -subj '/C=FI/O=Fintraffic/OU=EFTI/CN=Gate localhost'
-openssl req -new -key test-platform.key -out test-platform.csr -passin pass:$pwd -subj '/C=FI/O=Fintraffic/OU=EFTI/CN=Test platform'
+openssl req -new -key test-platform-fi-1.key -out test-platform-fi-1.csr -passin pass:$pwd -subj '/C=FI/O=Fintraffic/OU=EFTI/CN=Test platform fi 1'
+openssl req -new -key test-platform-fi-2.key -out test-platform-fi-2.csr -passin pass:$pwd -subj '/C=FI/O=Fintraffic/OU=EFTI/CN=Test platform fi 2'
 openssl req -new -key mock-platform.key -out mock-platform.csr -passin pass:$pwd -subj '/C=FI/O=Fintraffic/OU=EFTI/CN=Mock platform'
 openssl req -new -key aap.key -out aap.csr -passin pass:$pwd -subj '/C=FI/O=Fintraffic/OU=EFTI/CN=AAP'
 
 echo "Create certificates from requests"
 openssl x509 -req -days 365 -in gate-efti-localhost.csr -CA root-ca.crt -CAkey root-ca.key -CAcreateserial \
   -out gate-efti-localhost.crt -extfile ../gate-efti-localhost.cnf -passin pass:$pwd
-openssl x509 -req -days 365 -in test-platform.csr -CA platform-ca.crt -CAkey platform-ca.key -CAcreateserial \
-  -out test-platform.crt -extfile ../test-platform.cnf -passin pass:$pwd
+openssl x509 -req -days 365 -in test-platform-fi-1.csr -CA platform-ca.crt -CAkey platform-ca.key -CAcreateserial \
+  -out test-platform-fi-1.crt -extfile ../test-platform.cnf -passin pass:$pwd
+openssl x509 -req -days 365 -in test-platform-fi-2.csr -CA platform-ca.crt -CAkey platform-ca.key -CAcreateserial \
+  -out test-platform-fi-2.crt -extfile ../test-platform.cnf -passin pass:$pwd
 openssl x509 -req -days 365 -in mock-platform.csr -CA platform-ca.crt -CAkey platform-ca.key -CAcreateserial \
   -out mock-platform.crt -extfile ../mock-platform.cnf -passin pass:$pwd
 openssl x509 -req -days 365 -in aap.csr -CA aap-ca.crt -CAkey aap-ca.key -CAcreateserial \
   -out aap.crt -extfile ../aap.cnf -passin pass:$pwd
 
 echo "Create p12 files for chrome client certificates"
-openssl pkcs12 -export -inkey test-platform.key -in test-platform.crt -out test-platform.p12 -passin pass:$pwd -passout pass:$pwd
+openssl pkcs12 -export -inkey test-platform-fi-1.key -in test-platform-fi-1.crt -out test-platform-fi-1.p12 -passin pass:$pwd -passout pass:$pwd
+openssl pkcs12 -export -inkey test-platform-fi-2.key -in test-platform-fi-2.crt -out test-platform-fi-2.p12 -passin pass:$pwd -passout pass:$pwd
 openssl pkcs12 -export -inkey mock-platform.key -in mock-platform.crt -out mock-platform.p12 -passin pass:$pwd -passout pass:$pwd
 openssl pkcs12 -export -inkey aap.key -in aap.crt -out aap.p12 -passin pass:$pwd -passout pass:$pwd
