fddcbf39e512f7a4bbbd46f3750fed6fefed179f
Matti Uusitalo <matti.uusitalo@solita.fi>
2024-07-10 10:14:47 +0300

    EFTI-359 add another example of delivered consignment

--- backend/src/test/clj/fintraffic/efti/backend/service/edelivery_test.clj
+++ backend/src/test/clj/fintraffic/efti/backend/service/edelivery_test.clj
@@ -21,24 +21,48 @@
 
 (deftest uil-response
   (testing "Message is valid xml"
-    (let [payload (edelivery/uil-response {:uil
-                                           {:gateId "TODO EFTI-361",
-                                            :platformId "TODO EFTI-361",
-                                            :datasetId "da253ebf-4577-476a-99df-4e5f6bc6b750"},
-                                           :deliveryInformation nil,
-                                           :carrierAcceptanceDateTime "2024-07-10T09:46:15Z",
-                                           :deliveryEvent {:actualOccurrenceDateTime nil},
-                                           :mainCarriageTransportMovements
-                                           [{:dangerousGoodsIndicator false,
-                                             :transportModeCode 2,
-                                             :usedTransportMeans
-                                             {:identifier "RXCU", :registrationCountry {:id "DE"}}}],
-                                           :utilizedTransportEquipments
-                                           [{:categoryCode "T1",
-                                             :identifier "IAYY",
-                                             :sequenceNumeric 1,
-                                             :registrationCountry {:id "FI"},
-                                             :carriedTransportEquipments
-                                             [{:identifier "61ZU", :sequenceNumeric 1}]}]})]
-      (is (string? payload))
-      (validate-edelivery-payload! payload))))
+    (testing "Consignment under way"
+      (let [payload (edelivery/uil-response {:uil
+                                             {:gateId "TODO EFTI-361",
+                                              :platformId "TODO EFTI-361",
+                                              :datasetId "da253ebf-4577-476a-99df-4e5f6bc6b750"},
+                                             :deliveryInformation nil,
+                                             :carrierAcceptanceDateTime "2024-07-10T09:46:15Z",
+                                             :deliveryEvent {:actualOccurrenceDateTime nil},
+                                             :mainCarriageTransportMovements
+                                             [{:dangerousGoodsIndicator false,
+                                               :transportModeCode 2,
+                                               :usedTransportMeans
+                                               {:identifier "RXCU", :registrationCountry {:id "DE"}}}],
+                                             :utilizedTransportEquipments
+                                             [{:categoryCode "T1",
+                                               :identifier "IAYY",
+                                               :sequenceNumeric 1,
+                                               :registrationCountry {:id "FI"},
+                                               :carriedTransportEquipments
+                                               [{:identifier "61ZU", :sequenceNumeric 1}]}]})]
+        (is (string? payload))
+        (validate-edelivery-payload! payload)))
+
+    (testing "Delivered Consignment"
+      (let [payload (edelivery/uil-response {:uil
+                                             {:gateId "TODO EFTI-361",
+                                              :platformId "TODO EFTI-361",
+                                              :datasetId "da253ebf-4577-476a-99df-4e5f6bc6b750"},
+                                             :deliveryInformation "2024-07-11T10:46:15Z",
+                                             :carrierAcceptanceDateTime "2024-07-10T09:46:15Z",
+                                             :deliveryEvent {:actualOccurrenceDateTime "2024-07-11T10:46:15Z"},
+                                             :mainCarriageTransportMovements
+                                             [{:dangerousGoodsIndicator false,
+                                               :transportModeCode 2,
+                                               :usedTransportMeans
+                                               {:identifier "RXCU", :registrationCountry {:id "DE"}}}],
+                                             :utilizedTransportEquipments
+                                             [{:categoryCode "T1",
+                                               :identifier "IAYY",
+                                               :sequenceNumeric 1,
+                                               :registrationCountry {:id "FI"},
+                                               :carriedTransportEquipments
+                                               [{:identifier "61ZU", :sequenceNumeric 1}]}]})]
+        (is (string? payload))
+        (validate-edelivery-payload! payload)))))
